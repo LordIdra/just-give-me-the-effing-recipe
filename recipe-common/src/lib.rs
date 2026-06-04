560203a6d871f7a04500ef437e872edce72ba43e
LordIdra <percy.baker.0@gmail.com>
2026-06-05 00:31:30 +0100

    IT'S TIME TO PROFILE

--- recipe-common/src/lib.rs
+++ recipe-common/src/lib.rs
@@ -2,3 +2,10 @@ pub mod link;
 pub mod link_blacklist;
 pub mod recipe;
 
+#[macro_export]
+macro_rules! profile {
+    ($name:expr) => {
+        let _span = tracy_client::span!($name);
+    };
+}
+
