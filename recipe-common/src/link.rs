f9c56fa6e2701b9d1e639782dfa6420d96c7580e
LordIdra <percy.baker.0@gmail.com>
2026-06-29 20:19:53 +0100

    THAT WAS A STUPID MISTAKE

--- recipe-common/src/link.rs
+++ recipe-common/src/link.rs
@@ -264,7 +264,10 @@ async fn exists(mut redis_links: MultiplexedConnection, link: &str) -> Result<bo
 
 #[tracing::instrument(skip_all)]
 pub async fn poll_next_jobs(mut redis_links: MultiplexedConnection, count: usize) -> Result<Vec<String>, Error> {
-    let next_domains: Vec<String> = redis::cmd("SMEMBERS").arg(key_waiting_domains()).arg(count).query_async(&mut redis_links).await?;
+    let next_domains: Vec<String> = redis::cmd("SSCAN")
+        .arg(key_waiting_domains())
+        .arg(0)
+        .arg(count).query_async(&mut redis_links).await?;
     let next_domains_size = next_domains.len();
 
     trace!("{next_domains_size} domains available for next jobs");
